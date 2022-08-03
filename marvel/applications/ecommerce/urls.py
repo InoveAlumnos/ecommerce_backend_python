b18021b945aeecad225d12f48c05d63216ce9af5
julian-salinas <juliansalinasm@gmail.com>
2022-08-03 01:57:22 -0300

    feat: login-signup views

--- marvel/applications/ecommerce/urls.py
+++ marvel/applications/ecommerce/urls.py
@@ -19,7 +19,26 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
+from applications.ecommerce.views import *
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
+    path('base/', BaseView.as_view(), name='base'),
 
+    path('login/', auth_views.LoginView.as_view(
+                    template_name='ecommerce/login.html', 
+                    redirect_authenticated_user=True, 
+                    redirect_field_name='index'
+                    ), name='login'),
+
+    path('logout/', auth_views.LogoutView.as_view(
+                    next_page='/ecommerce/',  
+                    redirect_field_name='index'), name='logout'),
+         
+    path('signup/', register, name = 'register'),
+
+    # NOTE: Site pages
+    path('', IndexView.as_view(), name='index'),  # Index view
+    
 ]
--- marvel/applications/ecommerce/views.py
+++ marvel/applications/ecommerce/views.py
@@ -1,3 +1,74 @@
-from django.shortcuts import render
+#!/usr/bin/env python
 
-# Create your views here.
+from django import forms
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class BaseView(TemplateView):
+    '''
+    Base template
+    '''
+    template_name = 'ecommerce/base.html'
+
+
+class LoginUserView(TemplateView):
+    '''
+    Formulario de inicio de sesión.
+    '''
+    template_name = 'ecommerce/login.html'
+
+
+class UserForm(UserCreationForm):
+    '''
+    Formulario de creación de usuario.
+    '''
+
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'username',
+                  'email', 'password1', 'password2')
+
+
+def register(request):
+    '''
+    Registro de usuario.
+    
+    Redireccionamientos:
+        - información inválida: /signup
+        - registro exitoso: /login
+    '''
+
+    if request.method == 'POST':
+
+        form = UserForm(request.POST)
+
+        if form.is_valid():
+
+            # Guardar el formulario si este es válido
+            form.save()
+            
+            # Redirigin a página de inicio de sesión
+            return redirect('/ecommerce/login')
+
+    else:
+
+        # Crear un objeto de tipo formulario y enviarlo al contexto de renderización
+        form = UserForm()
+    
+    return render(request, 'ecommerce/signup.html', dict(form = form))
+
+
+class IndexView(TemplateView):
+    '''
+    Página principal del sitio.
+    '''
+
+    template_name = 'ecommerce/index.html'
